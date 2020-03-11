b4402bd4a47dd48342ba59ed9bc9d87f178e34a6
Ricardo Sorana <ricardosorana@gmail.com>
2020-03-10 22:21:11 -0300

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+app.get('/beers', (req, res) => {
+    const promise = punkAPI.getBeers();
+    promise.then(beersArray => {
+        console.log(beersArray);
+        res.render('beers', {"beersArray": beersArray});
+    })
+    .catch(() => console.log('Not found'));
+});
+
+app.get('/random-beer', (req, res) => {
+    const promise = punkAPI.getRandom();
+    promise.then(randomBeer => {
+        console.log(randomBeer);
+        res.render('random-beer', randomBeer[0]);
+    })
+    .catch(() => console.log('Not found'));
+});
+
+
+app.listen(3001, () => console.log('🏃‍ on port 3000'));
+
